d6f9227a1d1c7fbf0cc72974ea1bf5e696c6f8c5
Miguel Ángel Ortiz <tex@macbook-tex>
2013-11-04 10:18:22 +0100

    Añadido código para descubrir cual era el error que provocaba que la página estuviera en blanco.

--- html/index.php
+++ html/index.php
@@ -1,5 +1,22 @@
 <?php
 
+set_error_handler('errorHandler');
+
+function errorHandler( $errno, $errstr, $errfile, $errline, $errcontext)
+{
+  echo 'Into '.__FUNCTION__.'() at line '.__LINE__.
+  "\n\n---ERRNO---\n". print_r( $errno, true).
+  "\n\n---ERRSTR---\n". print_r( $errstr, true).
+  "\n\n---ERRFILE---\n". print_r( $errfile, true).
+  "\n\n---ERRLINE---\n". print_r( $errline, true).
+  "\n\n---ERRCONTEXT---\n".print_r( $errcontext, true).
+  "\n\nBacktrace of errorHandler()\n".
+  print_r( debug_backtrace(), true);
+}
+
+
+
+
 /*
  *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
