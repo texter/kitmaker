029b26db7522d5a66f3c2782384df47334efec84
Miguel Ángel Ortiz <tex@macbook-tex>
2013-11-06 19:31:17 +0100

    modificado para añadir los datos de contacto

--- html/application/views/pages/about.php
+++ html/application/views/pages/about.php
@@ -1,8 +1,13 @@
-Hola mundo...
-asdf
-asdf
-asdf
-asdf
-asd
-fasd
-f
+<section id="contacto">
+	<h2>Contacto</h2>
+	<ul>
+		<li><b>twitter:</b> <a href="https://twitter.com/M_a_ortiz" target="_blank">@m_a_ortiz</a></li>
+		<li><b>Facebook:</b> <a href="http://www.facebook.com/miguelangel.ortiziglesias" target="_blank">facebook.com/miguelangel.ortiziglesias</a></li>
+		<li><b>mail:</b> <a href="mailto:texter69@gmail.com">texter69@gmail.com</a></li>
+		<li><b>linkeid:</b> <a href="es.linkedin.com/pub/miguel-ángel-ortiz-iglesias/58/8b9/821" target="_blank">es.linkedin.com/miguel-ángel-ortiz-iglesias</a></li>
+		<li><b>infojobs:</b> <a href="http://www.infojobs.net/miguelangel-ortiz-4.prf" target="_blank">infojobs.net/miguelangel-ortiz</a></li>
+		<li><b>cuvitt:</b> <a href="https://es.cuvitt.com/professional/miguel-angel-ortiz-iglesias/" target="_blank">cuvitt/miguel-angel-ortiz-iglesias</a></li>
+		<li><b>Organizaciones actuales:</b> <a href="" target="_blank"></a></li>
+	</ul>
+	<p>Obtén el <a href="https://www.dropbox.com/s/hc595c3tu30vgq1/Curriculum%20Vitae%20de%20Miguel%20Angel%20Ortiz%20Iglesias.pdf">currículum en PDF</a>.</p>
+</section>
