16f8891eba16d55d9222b8c7307849fb46d31485
Miguel Ángel Ortiz <tex@macbook-tex>
2013-11-06 20:21:55 +0100

    añadido codigo para ver los fallos en php

--- html/application/controllers/pages.php
+++ html/application/controllers/pages.php
@@ -1,14 +1,16 @@
 <?php
+include_once('/home/tex/Documentos/acces_to_kitmaker/Blog/kitmaker/html/FuncionError.php');//Funcion para mostrar el error
 class Pages extends CI_Controller{
 	public function view($page='home'){
-		if (! file_exists('application/views/pages/'.$page.'.php)){
+		if (! file_exists('application/views/pages/'.$page.'.php')){
 			//Juas no está echa la pagina!!!
 			show_404();
 		}
+		
 		$data['title']=ucfirst($page);//Capitaliza la primera letra
 		
-		$this-> load->view('templates/header',$data);
-		$this-> load->view('pages/'.$page',$data);
-		$this-> load->view('templates/footer',$data);
+		$this-> load->view('templates/header', $data);
+		$this-> load->view('pages/'.$page, $data);
+		$this-> load->view('templates/footer', $data);
 	}
 }
